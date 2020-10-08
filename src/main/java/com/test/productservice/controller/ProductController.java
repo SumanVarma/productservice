831b26100ea69bf62163e495944bc41dfdd0b254
SumanVarma <sumanvarma83@gmail.com>
2020-10-08 09:51:51 +0530

    Updated

--- src/main/java/com/test/productservice/controller/ProductController.java
+++ src/main/java/com/test/productservice/controller/ProductController.java
@@ -3,7 +3,6 @@ package com.test.productservice.controller;
 import com.test.productservice.entity.Product;
 import com.test.productservice.exception.ResourceNotFoundException;
 import com.test.productservice.repository.ProductRepository;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,20 +23,20 @@ public class ProductController {
     ProductRepository productRepository;
 
     @GetMapping("/products")
-    public List<Product> getAllTodos() {
+    public List<Product> products() {
         List<Product> products = new ArrayList<>();
         productRepository.findAll().forEach(product -> products.add(product));
         return products;
     }
 
     @PostMapping("/products")
-    public Product createTodo(@Valid @RequestBody Product product) {
+    public Product createProduct(@Valid @RequestBody Product product) {
         product.setActive(true);
         return productRepository.save(product);
     }
 
     @GetMapping(value = "/products/{id}")
-    public ResponseEntity<Product> getTodoById(@PathVariable("id") long id) throws ResourceNotFoundException {
+    public ResponseEntity<Product> productById(@PathVariable("id") long id) throws ResourceNotFoundException {
         HttpStatus httpStatus = HttpStatus.OK;
         Product product = getProduct(id);
 
@@ -55,16 +54,17 @@ public class ProductController {
     }
 
     @PutMapping(value = "/products/{id}")
-    public ResponseEntity<Product> updateTodo(@PathVariable("id") Long id,
-                                              @Valid @RequestBody Product product) throws ResourceNotFoundException {
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id,
+                                                 @Valid @RequestBody Product product) throws ResourceNotFoundException {
         HttpStatus httpStatus = HttpStatus.OK;
         Product actualProduct = getProduct(id);
-
+        System.out.printf("product ", actualProduct);
         if (ObjectUtils.isEmpty(product)) {
             httpStatus = HttpStatus.NOT_FOUND;
             throw new ResourceNotFoundException("Product not found");
         } else {
-            BeanUtils.copyProperties(product, actualProduct);
+            // BeanUtils.copyProperties(product, actualProduct);
+            product.setId(id);
             productRepository.save(product);
         }
 
@@ -72,7 +72,7 @@ public class ProductController {
     }
 
     @DeleteMapping(value = "/products/{id}")
-    public ResponseEntity<?> deleteTodo(@PathVariable("id") Long id) throws ResourceNotFoundException {
+    public ResponseEntity<?> deleteProduct(@PathVariable("id") Long id) throws ResourceNotFoundException {
         HttpStatus httpStatus = HttpStatus.ACCEPTED;
         Product product = getProduct(id);
 
--- src/main/java/com/test/productservice/entity/Product.java
+++ src/main/java/com/test/productservice/entity/Product.java
@@ -1,23 +1,15 @@
 package com.test.productservice.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
-import java.sql.Timestamp;
+import java.util.Date;
 
 @Entity
 @Table(name = "product")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@SuperBuilder
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -34,10 +26,135 @@ public class Product {
     private BigDecimal price;
     private boolean active;
     private boolean primeAvailable;
-    @DateTimeFormat(pattern = "dd/MM/yyyy")
-    private Timestamp manufactureDate;
-    @DateTimeFormat(pattern = "dd/MM/yyyy")
-    private Timestamp expiryDate;
+    @JsonFormat(pattern = "dd/MM/yyyy", shape = JsonFormat.Shape.STRING, timezone = "UTC")
+    @Temporal(TemporalType.DATE)
+    @NotNull
+    private Date manufactureDate;
+    @JsonFormat(pattern = "dd/MM/yyyy", shape = JsonFormat.Shape.STRING, timezone = "UTC")
+    @Temporal(TemporalType.DATE)
+    @NotNull
+    private Date expiryDate;
 
+    public Product() {
+    }
 
+    public Product(Long id, @NotNull String productType, @NotNull String productName, String productSKU, String productBrand, BigDecimal weight, @NotNull BigDecimal price, boolean active,
+                   boolean primeAvailable, @NotNull Date manufactureDate, @NotNull Date expiryDate) {
+        this.id = id;
+        this.productType = productType;
+        this.productName = productName;
+        this.productSKU = productSKU;
+        this.productBrand = productBrand;
+        this.weight = weight;
+        this.price = price;
+        this.active = active;
+        this.primeAvailable = primeAvailable;
+        this.manufactureDate = manufactureDate;
+        this.expiryDate = expiryDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductSKU() {
+        return productSKU;
+    }
+
+    public void setProductSKU(String productSKU) {
+        this.productSKU = productSKU;
+    }
+
+    public String getProductBrand() {
+        return productBrand;
+    }
+
+    public void setProductBrand(String productBrand) {
+        this.productBrand = productBrand;
+    }
+
+    public BigDecimal getWeight() {
+        return weight;
+    }
+
+    public void setWeight(BigDecimal weight) {
+        this.weight = weight;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public boolean isPrimeAvailable() {
+        return primeAvailable;
+    }
+
+    public void setPrimeAvailable(boolean primeAvailable) {
+        this.primeAvailable = primeAvailable;
+    }
+
+    public Date getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate(Date manufactureDate) {
+        this.manufactureDate = manufactureDate;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", productType='" + productType + '\'' +
+                ", productName='" + productName + '\'' +
+                ", productSKU='" + productSKU + '\'' +
+                ", productBrand='" + productBrand + '\'' +
+                ", weight=" + weight +
+                ", price=" + price +
+                ", active=" + active +
+                ", primeAvailable=" + primeAvailable +
+                ", manufactureDate=" + manufactureDate +
+                ", expiryDate=" + expiryDate +
+                '}';
+    }
 }
